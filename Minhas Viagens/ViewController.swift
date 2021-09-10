cd614d38a4a783798dd6dabd010d46e55a7df3d8
Andre Linces <andrelinces@yahoo.com.br>
2021-09-10 19:55:43 -0300

    Adicionando mapa, configurações e permissões.

--- Minhas Viagens/ViewController.swift
+++ Minhas Viagens/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapa: MKMapView!
+    //Inicializando o objeto
+    var gerenciadorLocalizacao = CLLocationManager ()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configuraGerenciadorLocalizacao()
+        
+    }
+    
+    func configuraGerenciadorLocalizacao (){
+        
+        //Objeto será gerenciado pela própria classe viewController pois já está herdando de Cllocation
+        gerenciadorLocalizacao.delegate = self
+        //Definindo a precisão
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
+        //Solicitação de autorização
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        //Atuando a localização do usuário
+        gerenciadorLocalizacao.startUpdatingLocation()
+        
+    }
+    
+    
     }
 
 
-}
+
 
