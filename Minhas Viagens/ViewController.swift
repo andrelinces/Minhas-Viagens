9d62227543a70561033c86b318879299287a68d9
Andre Linces <andrelinces@yahoo.com.br>
2021-09-11 11:26:54 -0300

    Realizada criação das anotações (marcações ) no mapa.

--- Minhas Viagens/ViewController.swift
+++ Minhas Viagens/ViewController.swift
@@ -20,6 +20,34 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         configuraGerenciadorLocalizacao()
         
+        //Método reconhecedor de gestos, para identificar quando o usuário mantém a localização pressionada por 2 segundos.
+        let reconhecedorGestos = UILongPressGestureRecognizer(target: self, action: #selector( ViewController.marcar ) )
+        reconhecedorGestos.minimumPressDuration = 2
+        
+        mapa.addGestureRecognizer( reconhecedorGestos )
+    }
+    //Método criado para marcar o ponto pressionado pelo usuário
+    @objc func marcar (gesture: UIGestureRecognizer){
+        //Faz tratamento de capturar apenas o momento em que satisfaz a condição dos 2 segundos pressionado, gerando apenas uma marcação.
+        if gesture.state == UIGestureRecognizer.State.began {
+            //print apenas para testar se o método está funcionando corretamente.
+            print("Pressionado !!")
+        
+            //Recupera as coordenadas do ponto que foi pressionado (selecionado)
+            let pontoSelecionado = gesture.location( in: self.mapa)
+            let coordenadas = mapa.convert(pontoSelecionado, toCoordinateFrom: self.mapa)
+            
+            //Exibe anotação com os dados do endereço
+            let anotacao = MKPointAnnotation()
+            
+            anotacao.coordinate.latitude = coordenadas.latitude
+            anotacao.coordinate.longitude = coordenadas.longitude
+            anotacao.title = "Pressionei aqui !"
+            anotacao.title = "Estou aqui !"
+            
+            mapa.addAnnotation( anotacao )
+            
+        }
     }
     
     func configuraGerenciadorLocalizacao (){
@@ -35,8 +63,38 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
     }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status != .authorizedWhenInUse {
+            
+            let alertController = UIAlertController(title: "Permissão de Localização!!",
+                                                    message: "Necessário permitir acesso a sua localização para o funcionamento do APP!",
+                                                    preferredStyle: .alert)
+            
+            
+            let acaoConfiguracoes = UIAlertAction(title: "Abrir Configurações!", style: .default) { ( alertaConfiguracoes ) in
+                
+                if let configuracoes = NSURL( string: UIApplication.openSettingsURLString ){
+                    UIApplication.shared.open( configuracoes as URL)
+                    
+                }
+                }
+                
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            alertController.addAction( acaoConfiguracoes )
+            alertController.addAction(acaoCancelar)
+            
+            present(alertController, animated: true, completion: nil)
+            
+            }
+            
+        }
+        }
+        
+        
     
-    }
+    
+    
 
 
 
